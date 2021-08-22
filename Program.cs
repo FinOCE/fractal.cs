f41cde2d7238daae472ea1f345213a26e065f712
Finley Sherwood <me@itsf.in>
2021-08-23 01:13:20 +1000

    Use practical render vars

--- Program.cs
+++ Program.cs
@@ -8,12 +8,12 @@ namespace Fractal
     {
         public static void Main(string[] args)
         {
-            int iterations = 100;
-            int width = 256;
-            int height = 256;
-            double x = -0.518d;
-            double y = -0.5215d;
-            int zoom = 1;
+            int iterations = 2000;
+            int width = 2048;
+            int height = 2048;
+            double x = -0.518;
+            double y = -0.5215;
+            int zoom = 1048576;
             var color = Color.Red;
 
             RenderProperties renderProperties = new RenderProperties((width, height), (x, y), zoom, iterations, color);
